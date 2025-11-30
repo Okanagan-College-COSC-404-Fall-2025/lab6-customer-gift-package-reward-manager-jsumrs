1c2022c0e98ea8f5b793b4aa41b6c79b73a08f36
jsumrs <github@jasoncodes.ca>
2025-11-30 11:47:08 -0800

    Implement choose_gift_package function. Case statement to satisfy lab requirement

--- scripts/Q3.sql
+++ scripts/Q3.sql
@@ -1,10 +1,33 @@
 CREATE OR REPLACE PACKAGE customer_manager AS
-    FUNCTION get_total_purchase(customer_id NUMBER) RETURN NUMBER;
+    FUNCTION get_total_purchase(customer_id_in NUMBER) RETURN NUMBER;
     PROCEDURE assign_gifts_to_all;
 END customer_manager;
 
 CREATE OR REPLACE PACKAGE BODY customer_manager AS
-    FUNCTION choose_gift_package(total_purchase_in IN NUMBER) RETURN NUMBER IS
-    BEGIN
+    FUNCTION get_total_purchase(customer_id_in NUMBER) RETURN NUMBER IS
+        DECLARE
+            v_total NUMBER;
+        BEGIN
+            -- Find all orders which match the customer id
+            -- -- Sum the items within each order and add that to a running total
+            select SUM(oi.unit_price * oi.quantity)
+            into v_total
+            from order_items oi
+            join orders o on o.order_id = oi.order_id
+            where o.customer_id = customer_id_in;
 
-    END choose_gift_package
+            RETURN v_total;
+    END get_total_purchase;
+
+    FUNCTION choose_gift_package(total_purchase_in IN NUMBER) RETURN NUMBER IS
+        BEGIN
+    END choose_gift_package;
+
+END choose_gift_package
+
+
+SELECT SUM(oi.unit_price * oi.quantity)
+  FROM order_items oi
+  JOIN orders o 
+  ON o.order_id = oi.order_id
+  WHERE o.customer_id = 1;
